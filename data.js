960923c12ff97a6a4016c15de3875e70e32c1960
Shreeti <114981873+shivah12@users.noreply.github.com>
2024-01-16 22:00:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Movie description',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Movie description',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Movie description',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Movie description',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Movie description',
+        image: 'images/slider 5.PNG'
+    }
+]
